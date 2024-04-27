9c909e15838f39212efe7bedbb04603932c695f6
Gleb K <gleb.kapustin.98@mail.ru>
2024-04-27 14:12:45 +0300

    changed organization

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -10,16 +10,11 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowsScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowsScene)
-        
-     //   let servicesAssembly = ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl())
-        
         let tabBarController = controllersFactory.setupTabBarController()
-        //TabBarController(controllersFactory: controllersFactory, servicesAssembly: servicesAssembly)
 
         window.rootViewController = UserDefaults.standard.isOnBoarded ? tabBarController : OnBoardingController()
-
-        self.window = window
-
         window.makeKeyAndVisible()
+        
+        self.window = window
     }
 }
